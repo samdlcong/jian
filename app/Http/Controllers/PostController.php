66dc428ae5f60ed1fffd8de4c8c563b05f01b4d9
samdlcong <samnew@126.com>
2018-12-14 17:22:12 +0800

    add route

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    // 列表
+    public function index()
+    {
+        return view("post/index");
+    }
+
+
+    // 创建页面
+    public function create()
+    {
+
+    }
+
+    // 创建逻辑
+    public function store()
+    {
+
+    }
+
+    // 编辑页面
+    public function edit()
+    {
+
+    }
+
+    // 编辑逻辑
+    public function update()
+    {
+
+    }
+
+    // 创建逻辑
+    public function delete()
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,14 @@
 |
 */
 // 文章列表页
-Rotue::get('/posts','\App\Http\Controllers\PostController@index');
+Route::get('/posts','\App\Http\Controllers\PostController@index');
 // 文章详情页
-Rotue::get('/posts/{post}','\App\Http\Controllers\PostController@show');
+Route::get('/posts/{post}','\App\Http\Controllers\PostController@show');
 // 创建文章
-Rotue::get('/posts/create','\App\Http\Controllers\PostController@create');
-Rotue::post('/posts','\App\Http\Controllers\PostController@store');
+Route::get('/posts/create','\App\Http\Controllers\PostController@create');
+Route::post('/posts','\App\Http\Controllers\PostController@store');
+// 编辑文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
+// 删除文章
+Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
