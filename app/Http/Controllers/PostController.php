b9361c812a607367ccbe158f64d04c4d697ee64f
samdlcong <samnew@126.com>
2018-12-16 23:49:21 +0800

    add validation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,10 +29,16 @@ class PostController extends Controller
     // 创建逻辑
     public function store()
     {
+        // 验证
+        $this->validate(request(),[
+            'title'=>'required|string|max:100|min:5',
+            'content'=> 'required|string|min:10',
+        ]);
+
         $params = request(['title','content']);
         Post::create($params);
 
-        //dd(request()->all());
+        return redirect("/posts");
     }
 
     // 编辑页面
