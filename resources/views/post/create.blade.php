bead3a35341451cf0580fcd22cf12bb7c0406df3
samdlcong <samnew@126.com>
2018-12-18 11:09:40 +0800

    新增error模板

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -11,14 +11,7 @@
                     <label>内容</label>
                     <textarea id="content"  style="height:400px;max-height:500px;" name="content" class="form-control" placeholder="这里是内容"></textarea>
                 </div>
-                @if (count($errors) > 0)
-                <div class="alert alert-danger alert-dismissible" role="alert">
-                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    @foreach($errors->all() as $error)
-                        <li>{{$error}}</li>
-                    @endforeach
-                </div>
-                @endif
+                @include("layout.error")
                 <button type="submit" class="btn btn-default">提交</button>
             </form>
             <br>
--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,8 +1,8 @@
 @extends("layout.main")
 @section("content")
         <div class="col-sm-8 blog-main">
-            <form action="/posts/{{$post->id}}}" method="POST">
-                {{method_field('put')}}
+            <form action="/posts/{{$post->id}}" method="POST">
+                {{method_field('PUT')}}
                 {{csrf_field()}}
                 <div class="form-group">
                     <label>标题</label>
@@ -12,6 +12,7 @@
                     <label>内容</label>
                     <textarea id="content" name="content" class="form-control" style="height:400px;max-height:500px;"  placeholder="这里是内容">{{$post->content}}</textarea>
                 </div>
+                @include("layout.error")
                 <button type="submit" class="btn btn-default">提交</button>
             </form>
             <br>
