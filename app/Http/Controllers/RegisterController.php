f91d7db34bb4e61f4d95e176dc08e74ea6576d23
samdlcong <samnew@126.com>
2018-12-18 16:16:45 +0800

    注册页面

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class RegisterController extends Controller
 {
@@ -15,6 +16,18 @@ class RegisterController extends Controller
     //注册行为
     public function register()
     {
+        $this->validate(request(), [
+            'name'=>'required|min:3|unique:users,name',
+            'email'=>'required|unique:users,email|email',
+            'password'=>'required|min:6|max:32|confirmed',
+        ]);
 
+        $name = request('name');
+        $email = request('email');
+        $password = bcrypt(request('password'));
+
+        $user = User::create(compact('name', 'email', 'password'));
+
+        return redirect('/login');
     }
 }
