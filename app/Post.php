ac3310fa23192480c18d115922913a512f2c3fa4
samdlcong <samnew@126.com>
2018-12-20 21:52:03 +0800

    添加-m 修改控制器

--- app/Post.php
+++ app/Post.php
@@ -4,12 +4,29 @@ namespace App;
 
 use App\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 
 class Post extends Model
 {
     use SoftDeletes;
+    use Searchable;
 
     //protected $dates=['deleted_time'];
+
+    // 定义索引里面的type
+    public function searchableAs()
+    {
+        return "post";
+    }
+
+    // 定义有哪些字段需要搜索
+    public function searchableArray()
+    {
+        return [
+            'title'=> $this->title,
+            'content'=> $this->content,
+        ];
+    }
 
     // 关联用户
     public function user()
