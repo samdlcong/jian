97e2162bb1b98755b4ca5aca7392f924c48062f1
samdlcong <samnew@126.com>
2018-12-26 13:24:16 +0800

    add Topic model

--- app/Topic.php
+++ app/Topic.php
@@ -6,5 +6,15 @@ use App\Model;
 
 class Topic extends Model
 {
-    //
+    // 属于这个专题的所有文章
+    public function posts()
+    {
+        $this->belongsToMany(Post::class, 'post_topics', 'topic_id', 'post_id');
+    }
+
+    // 专题的文章数
+    public function postTopics()
+    {
+        $this->hasMany(PostTopic::class, 'topic_id');
+    }
 }
