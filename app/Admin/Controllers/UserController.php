1ad05fa6c801e93fec51f53e2786de839b282b45
samdlcong <samnew@126.com>
2018-12-27 21:58:48 +0800

    管理员界面

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    // 管理员列表页面
+    public function index()
+    {
+        return view('admin/user/index');
+    }
+
+    // 管理员创建页面
+    public function create()
+    {
+        return view('admin/user/create');
+    }
+
+    // 创建
+    public function store()
+    {
+
+    }
 }
