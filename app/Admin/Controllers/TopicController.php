1ec604d186c1f4db6c3323eb5f17b1a82ea83836
samdlcong <samnew@126.com>
2019-01-01 19:15:24 +0800

    add TopicController

---
+++ app/Admin/Controllers/TopicController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class TopicController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destory()
+    {
+        
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -46,6 +46,10 @@ Route::group(['prefix'=>'admin'], function(){
             Route::get('/posts', '\App\Admin\Controllers\PostController@index');
             Route::post('/posts/{post}/status', '\App\Admin\Controllers\PostController@status');
         });
+
+        Route::group(['middleware'=>'can:topic'], function(){
+           Route::resource('topics', '\App\Admin\Controllers\TopicController', ['only'=>['index', 'create', 'store', 'destory']]);
+        });
     });
 
 });
